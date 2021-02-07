b499f6267542f469b4bc753e84ad2227e264ed71
Said <korkmazzsaid@gmail.com>
2021-02-07 17:25:02 +0300

    Login tasarımına başlandı

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 import 'models/product.dart';
 import 'models/teacher.dart';
 import 'models/investor.dart';
@@ -5,3 +7,14 @@ import 'models/investor.dart';
 List<Product> cart;
 Teacher currentTeacher;
 Investor currentInvestor;
+
+
+/* COLORS */
+
+Color colorPrimaryShade = Color(0xff2a9479);
+Color colorPrimary = Color(0xff3cd3ad);
+Color colorPrimaryTint = Color(0xff77e0c6);
+
+Color colorSecondaryShade = Color(0xff358189);
+Color colorSecondary = Color(0xff4cb8c4);
+Color colorSecondaryTint = Color(0xff82cdd6);
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterio/core/locator.dart';
+import 'package:flutterio/globals.dart';
 import 'package:flutterio/view_models/login_model.dart';
 import 'package:provider/provider.dart';
 
@@ -16,71 +17,107 @@ class LoginPage extends StatelessWidget {
       child: Consumer<LoginModel>(
         builder: (BuildContext context, LoginModel model, Widget child) =>
             Scaffold(
+
           body: SingleChildScrollView(
             child: Stack(
               children: [
                 Container(
+                  height:size.height,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [colorPrimary, colorSecondary])
+                  ),
+
+                ),
+                Container(
+                  height:size.height,
+                  width: size.width,
+
                   padding: EdgeInsets.all(8),
                   child: model.busy
                       ? Center(child: CircularProgressIndicator())
-                      : Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: size.height * 0.5,
-                              ),
-                              TextField(
-                                  controller: _emailController,
-                                  keyboardType: TextInputType.emailAddress,
-                                  decoration: InputDecoration(
-                                    prefixIcon: Icon(
-                                      Icons.mail,
-                                      color: Colors.red,
-                                    ),
-                                    hintText: 'Mail adresi',
-                                    prefixText: ' ',
-                                  )),
-                              SizedBox(
-                                height: size.height * 0.02,
-                              ),
-                              TextField(
-                                  controller: _passwordController,
-                                  obscureText: true,
-                                  decoration: InputDecoration(
-                                    prefixIcon: Icon(
-                                      Icons.vpn_key,
-                                      color: Colors.red,
-                                    ),
-                                    hintText: 'Parola',
-                                    prefixText: ' ',
-                                  )),
-                              SizedBox(
-                                height: size.height * 0.04,
-                              ),
-                              InkWell(
-                                onTap: () async => await model.signIn(
-                                    _emailController.text,
-                                    _passwordController.text),
+                      : Center(
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Container(
 
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Colors.red,
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(10))),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Center(
-                                        child: Text(
-                                      "Giriş yap",
-                                      style: TextStyle(color: Colors.white),
-                                    )),
+                              height:size.height*.5,
+                              width: size.width*.85,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(.25),
+                                borderRadius: BorderRadius.all(Radius.circular(20)),
+                                //boxShadow: [BoxShadow(color:Colors.grey.withOpacity(.75), blurRadius: 10, spreadRadius: 2)]
+
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+
+                                      TextField(
+                                          controller: _emailController,
+                                          keyboardType: TextInputType.emailAddress,
+                                          decoration: InputDecoration(
+                                            prefixIcon: Icon(
+                                              Icons.mail,
+                                              color: colorPrimaryShade,
+                                            ),
+                                            hintText: 'Mail adresi',
+                                            prefixText: ' ',
+                                            hintStyle: TextStyle(color: Colors.white)
+                                          )),
+                                      SizedBox(
+                                        height: size.height * 0.02,
+                                      ),
+                                      TextField(
+                                          controller: _passwordController,
+                                          obscureText: true,
+                                          decoration: InputDecoration(
+                                            prefixIcon: Icon(
+                                              Icons.vpn_key,
+                                              color: colorPrimaryShade,
+                                            ),
+                                            hintText: 'Parola',
+                                            prefixText: ' ',
+                                              hintStyle: TextStyle(color: Colors.white)
+                                          )),
+                                      SizedBox(
+                                        height: size.height * 0.04,
+                                      ),
+                                      InkWell(
+                                        onTap: () async => await model.signIn(
+                                            _emailController.text,
+                                            _passwordController.text),
+
+                                        child: Container(
+                                          padding: EdgeInsets.symmetric(vertical: 8),
+                                          decoration: BoxDecoration(
+                                            border: Border.all(color:Colors.white,width: 2),
+                                              //color: colorPrimaryShade,
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(30))),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(10.0),
+                                            child: Center(
+                                                child: Text(
+                                              "Giriş yap",
+                                              style: TextStyle(color: Colors.white, fontSize: 17),
+                                            )),
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
-                              )
-                            ],
+                              ),
+                            ),
                           ),
-                        ),
+                      ),
                 ),
               ],
             ),
