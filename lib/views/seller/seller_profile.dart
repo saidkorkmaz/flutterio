da65a2805a18e62d3c4caff70dbbfbda4d7c71c1
Said <korkmazzsaid@gmail.com>
2021-02-07 18:08:37 +0300

    Seller profil tasarlandı

--- lib/views/seller/seller_profile.dart
+++ lib/views/seller/seller_profile.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterio/core/locator.dart';
-import 'package:flutterio/view_models/login_model.dart';
 import 'package:flutterio/view_models/product_model.dart';
-import 'package:provider/provider.dart';
+
+import '../../globals.dart';
 
 class SellerProfile extends StatefulWidget {
   @override
@@ -40,13 +40,30 @@ class _SellerProfileState extends State<SellerProfile> {
                         List<DocumentSnapshot> documents = snapshot.data.docs;
                         return Column(
                           children: [
-                            Text("Firma adı"),
                             TextField(
                               controller: _nameEditingController,
                               decoration: InputDecoration(
-                                  hintText: documents[0]["MARKET_NAME"]
+                                  hintText: documents[0]["MARKET_NAME"],
+                                prefixIcon: Icon(Icons.person, color:colorSecondaryTint)
                               ),
                             ),
+
+                            TextField(
+                              controller: _nameEditingController,
+                              decoration: InputDecoration(
+                                  hintText: documents[0]["PHONE"],
+                                  prefixIcon: Icon(Icons.phone, color:colorSecondaryTint)
+                              ),
+                            ),
+
+                            TextField(
+                              controller: _nameEditingController,
+                              decoration: InputDecoration(
+                                  hintText: documents[0]["ADDRESS"],
+                                  prefixIcon: Icon(Icons.home, color:colorSecondaryTint)
+                              ),
+                            ),
+
                           ],
                         );
                       }
@@ -62,7 +79,7 @@ class _SellerProfileState extends State<SellerProfile> {
                    child: Container(
                      height: size.height*0.05,
                      decoration: BoxDecoration(
-                         color: Colors.red,
+                         color: colorSecondaryShade,
                          borderRadius: BorderRadius.all(
                              Radius.circular(10))),
                      child: Padding(
